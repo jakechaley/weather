882c16d972150c61253a1a9bfd18b890ce5bbe35
jakechaley <jakechaley@gmail.com>
2022-02-07 11:25:28 -0800

    add wind speed, current conditions, sunrise and sunset to project results

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
     $('#location').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`;
+
 
 
     request.onreadystatechange = function() {
@@ -24,7 +25,11 @@ $(document).ready(function() {
 
     function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The Temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The Temperature in Fahrenheit is ${response.main.temp} degrees.`);
+      $('.showWindSpeed').text(`The wind speed is ${response.wind.speed} MPH`);
+      $('.showConditions').html(`The current conditions are: ${response.weather[0].description}. <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png" alt="weather-icon">`);
+      $('.showSunrise').text(`The sun will rise at ${new Date(response.sys.sunrise * 1000).toLocaleTimeString("en-US")}`);
+      $('.showSunset').text(`The sun will set at ${new Date(response.sys.sunset * 1000).toLocaleTimeString("en-US")}`);
     }
   });
 });
